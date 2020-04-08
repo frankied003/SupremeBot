a433478edb8df6d7017db2ecaf9ce506fc5be6e4
ckubelle <ckubelle@stevens.edu>
2020-04-07 22:48:48 -0400

    addtoCartv1

--- backEnd/backend.js
+++ backEnd/backend.js
@@ -19,6 +19,39 @@ console.log(productsJson);
 
 }
 
+
+function addItemToCart(){
+
+        axios.post(
+            
+        url="https://www.supremenewyork.com/shop/173091/add.json",
+
+        data = {
+            "st": 26762,
+            "s": 76982,
+            "qty": "1"
+            }, 
+
+        headers = {
+            'User-Agent': 'Mozilla/5.0 (iPhone; CPU iPhone OS 12_2 like Mac OS X) AppleWebKit/605.1.15 (KHTML, like Gecko) Mobile/15E148',
+            'Content-Type': 'application/x-www-form-urlencoded'
+            },
+    
+        )
+        .then((response) => {
+            console.log(response);
+          }, (error) => {
+            console.log(error);
+          });
+
+    
+
+}
+
+       
+    
+
+
 getSupremeProducts();
 
-
+addItemToCart();

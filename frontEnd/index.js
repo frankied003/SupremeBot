28753d6cc6572074e0443430bf3ebc190e9e17f6
Spencer Drucker <spencerd6637@gmail.com>
2020-04-14 13:08:18 -0400

    Added test and clear functions to index.js

--- frontEnd/index.js
+++ frontEnd/index.js
@@ -1,1 +1,26 @@
-const input = document.querySelectorAll(".inputs");
+
+function funcClear(){
+    document.getElementById("supForm").reset()
+}
+
+function Test(){
+    document.getElementById("Fname").value= "John"
+    document.getElementById("Lname").value= "Smith"
+    document.getElementById("Phone").value= "1234567890"
+    document.getElementById("Email").value= "Jsmith@domain.com"
+    document.getElementById("Saddress").value= "123 Main Street"
+    document.getElementById("Zip").value= "12345"
+    document.getElementById("country").value= "United States of America"
+    document.getElementById("state").value= "NJ"
+    document.getElementById("cardtype").value= "AE"
+    document.getElementById("Cname").value= "JOHN H SMTIH"
+    document.getElementById("Cnumber").value= "1234567890101112"
+    document.getElementById("CVV").value= "123"
+    document.getElementById("cardmonth").value= "January"
+    document.getElementById("cardyear").value= "2035"
+    document.getElementById("kword").value= "Jacket"
+    document.getElementById("delay").value= "120"
+    document.getElementById("color").value= "C2"
+    document.getElementById("size").value= "L"
+}
+
